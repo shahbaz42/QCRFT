93fc1188cde068bcc2060d5151a0fffd9860dfad
Shahbaz <shahbaz_ali@outlook.in>
2023-02-15 00:01:29 +0530

    add user model

---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const user = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    picture: {
+        type: String,
+        required: true
+    },
+    access_token: {
+        type: String,
+    },
+    refresh_token: {
+        type: String,
+    },
+    scope: {
+        type: String,
+    },
+    token_type: {
+        type: String,
+    },
+    expiry_date: {
+        type: Number,
+    }
+});
+
+module.exports = mongoose.model('User', user);
+
