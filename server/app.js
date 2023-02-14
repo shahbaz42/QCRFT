8e9718bd2bd9f5756e213128d770a1963307fbb3
Shahbaz <shahbaz_ali@outlook.in>
2023-02-14 22:34:57 +0530

    use morgan and cors

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const authRouter = require("./routes/authRouter");
 
 const app = express();
 
-app.use(express.json());
+app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.status(200).send("Server is up an running");
