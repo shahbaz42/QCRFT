010de60392d85ceee9aa0351472ebf83b127f991
Shahbaz <shahbaz_ali@outlook.in>
2023-02-16 01:19:36 +0530

    write code for placing form creation request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import { useState } from 'react';
 import './App.css';
+import { useRef } from 'react';
+import axios from 'axios';
 
 function App() {
   const [user, setUser] = useState({});
   const [loggedIn, setLoggedIn] = useState(false);
+  const [buttonName, setButtonName] = useState("Create Form");
+  const [formURL, setFormURL] = useState('');
+  const inputRef = useRef(null);
+  const inputNumRef = useRef(null);
 
   const handleLogout = () => {
     setLoggedIn(false);
     setUser({});
+  };
+
+  const placeRequest = () => {
+    setButtonName("Creating Form...")
+    const url = 'http://localhost:3001/api/quiz/createQuiz';
+    const data = {
+      url: inputRef.current.value,
+      num: inputNumRef.current.value,
+    };
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('token'),
+    };
+    axios.post(url, data, {headers})
+      .then((res) => {
+        setButtonName("Create Another Form");
+        setFormURL(res.data);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // const url = 'http://localhost:3001/api/auth/profile';
+    // const headers = {
+    //   'Content-Type': 'application/json',
+    //   'Authorization': 'Bearer ' + localStorage.getItem('token'),
+    // };
+
+    // axios.get(url, {headers})
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+
   };
 
   const handleAuthorization = () => {
@@ -23,7 +66,9 @@ function App() {
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         xhr.onload = () => {
-          console.log(xhr.responseText);
+          const resp = JSON.parse(xhr.responseText);
+          localStorage.setItem('token', resp.token);
+          setLoggedIn(true);
         };
         xhr.send('code=' + code);
       },
@@ -36,10 +81,17 @@ function App() {
       <div>
         { loggedIn &&
           <div>
-            <img src={user.picture} alt="user_pic" ></img>
+            {/* <img src={user.picture} alt="user_pic" ></img> */}
             <h2>Welcome, {user.name}</h2>
             <h3>{user.email}</h3>
             <button onClick={(e) => {handleLogout(e)}}>Sign Out</button>
+            <br />
+            <br />
+            <br />
+            <input ref={inputRef} placeholder="Video URL" type="text" /><br />
+            <input ref={inputNumRef} placeholder="Number of Questions" type="number" /><br />
+            <button onClick={(e) => {placeRequest(e)}}> {buttonName} </button>
+            { formURL && <a href={formURL}> {formURL}</a> }
           </div>
         }
         { !loggedIn && 
