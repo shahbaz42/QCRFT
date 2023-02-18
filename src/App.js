a9c84ecb3e5ffef0614dc550d038e6820c45ab22
Shahbaz <shahbaz_ali@outlook.in>
2023-02-19 02:05:52 +0530

    wrap in AuthProvider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import { AuthProvider } from './context/AuthContext';
 import LandingPage from './pages/LandingPage';
-import CraftForm from './pages/CraftForm' ;
+import CraftForm from './pages/CraftForm';
+import PrivateRoute from './utils/PrivateRoute';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<LandingPage/>} />
-      <Route path='/craft' element={<CraftForm/>} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route exact element={<PrivateRoute />}>
+          <Route path='/craft' element={<CraftForm />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 }
 
