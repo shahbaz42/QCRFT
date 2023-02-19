b820174448a59c00280aa4bca5fe999de014db86
Shahbaz <shahbaz_ali@outlook.in>
2023-02-19 14:50:55 +0530

    add function to generate more questions

--- src/Components/DividerWithButton.js
+++ src/Components/DividerWithButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { PlusSmallIcon } from '@heroicons/react/24/solid'
 
-export default function DividerWithButton() {
+export default function DividerWithButton(props) {
     return (
         <div className="relative my-3 drop-shadow-md">
             <div className="absolute inset-0 flex items-center" aria-hidden="true">
@@ -9,6 +9,7 @@ export default function DividerWithButton() {
             </div>
             <div className="relative flex justify-center">
                 <button
+                    onClick={() => { props.addQn(1)}}
                     type="button"
                     className="inline-flex items-center shadow-sm px-4 py-1.5 mx-2 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
@@ -16,6 +17,7 @@ export default function DividerWithButton() {
                     <span>1 Qn</span>
                 </button>
                 <button
+                    onClick={() => { props.addQn(2)}}
                     type="button"
                     className="inline-flex items-center shadow-sm px-4 py-1.5 mx-2 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
@@ -23,6 +25,7 @@ export default function DividerWithButton() {
                     <span>2 Qns</span>
                 </button>
                 <button
+                    onClick={() => { props.addQn(3)}}
                     type="button"
                     className="inline-flex items-center shadow-sm px-4 py-1.5 mx-2 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
