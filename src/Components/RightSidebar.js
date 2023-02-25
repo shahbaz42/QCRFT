369d8209209af9e96318cd4ce1361f4b62325fd1
Shahbaz <shahbaz_ali@outlook.in>
2023-02-25 15:17:31 +0530

    decrease the padding of the right sidebar

--- src/Components/RightSidebar.js
+++ src/Components/RightSidebar.js
@@ -5,17 +5,18 @@ import { useApp } from '../context/AppContext'
 import { MicrophoneIcon } from '@heroicons/react/24/outline'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import useWindowSize from '../hooks/useWindowSize';
+import Dropdown from './FormComponents/Dropdown'
 
 const SetReferenceSidebar = () => {
     const urlRef = useRef(null);
     const { token } = useAuth();
     const { subtitles, setSubtitles } = useApp();
-    const [ loading, setLoading ] = useState(false);
-    const [ sidebarOpen, setSidebarOpen ] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [sidebarOpen, setSidebarOpen] = useState(true);
     const { width } = useWindowSize();
 
     useEffect(() => {
-        if(width < 1536){
+        if (width < 1536) {
             setSidebarOpen(false);
         } else {
             setSidebarOpen(true);
@@ -66,17 +67,17 @@ const SetReferenceSidebar = () => {
                     Reference
                 </div>
             </div> */}
-            <div className="right-sidebar bg-white h-screen relative drop-shadow-xl hover:drop-shadow-2xl" style={{width:`${sidebarOpen ?'540px':'0'}`, transition:"width 0.4s" }}>
+            <div className="right-sidebar bg-white h-screen relative drop-shadow-xl hover:drop-shadow-2xl" style={{ width: `${sidebarOpen ? '540px' : '0'}`, transition: "width 0.4s" }}>
                 <div className='relative inline-block rotate-90' style={{ right: "68px", top: "100px" }}>
-                    <div 
+                    <div
                         className='bg-white p-2 text-lg text-center font-medium text-gray-600 drop-shadow-md hover:drop-shadow-xl hover:cursor-pointer'
-                        style={{width : "100px", height: "46px"}}
+                        style={{ width: "100px", height: "46px" }}
                         onClick={() => { setSidebarOpen(!sidebarOpen) }}
                     >
                         Reference
                     </div>
                 </div>
-                <div className='relative bg-white block w-full  border-gray-300 border-dashed p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+                <div className='relative bg-white block w-full  border-gray-300 border-dashed p-6 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
                     <div>
                         <div className="mt-1">
                             <div className='inline-block' style={{ width: "calc(100% - 85px - 45px - 40px )" }}>
@@ -94,6 +95,7 @@ const SetReferenceSidebar = () => {
                         <div className="mt-3">
                             <textarea className="dashed-textarea-black hideScroll" value={subtitles} onChange={textAreaHandler} style={{ height: "38vh" }} />
                         </div>
+                        <ParametersPane />
                     </div>
                 </div>
 
@@ -102,8 +104,40 @@ const SetReferenceSidebar = () => {
     )
 }
 
+export function ParametersPane() {
+    const { 
+        setDifficulty,
+        setOptions,
+        setCreativityLevel,
+        setLength,
+        setTone 
+    } = useApp();
+
+    return (
+        <>
+            <div className='mt-1 grid grid-cols-2 gap-4'>
+                <div className='mt-1'>
+                    <Dropdown onChange={(e) => {setDifficulty(e.target.value)}} label="Difficulty " menu={[{ name: "Easy", value: "easy" }, { name: "Medium", value: "medium" }, { name: "Hard", value: "hard" }]} />
+                </div>
+                <div className='mt-1'>
+                    <Dropdown onChange={(e) => {setOptions(e.target.value)}} label="Options " menu={[{ name: "Four", value: "four" }, {name : "Two", value: "two"}, { name: "Three", value: "three" }, { name: "Five", value: "five" }]} />
+                </div>
+                <div className='mt-1'>
+                    <Dropdown onChange={(e) => {setCreativityLevel(e.target.value)}} label="Creavity Level" menu={[{ name: "Optimal", value: "optimal" }, { name: "None", value: "None" }, { name: "Little Creative", value: "little creative" }, { name: "Creative", value: "creative" }, { name: "Highly creative", value: "highly creative" }]} />
+                </div>
+                <div className='mt-1'>
+                    <Dropdown onChange={(e) => {setLength(e.target.value)}} label="Length" menu={[{ name: "Short", value: "short" }, { name: "Long", value: "long" }]} />
+                </div>
+                <div className='mt-1'>
+                    <Dropdown onChange={(e) => {setTone(e.target.value)}} label="Tone" menu={[{ name: "Formal", value: "formal" }, { name: "Casual", value: "Casual" }]} />
+                </div>
+            </div>
+        </>
+    )
+}
+
 export default function RightSidebar() {
-    const {quizCreated} = useApp();
+    const { quizCreated } = useApp();
     return (
         <>
             {quizCreated &&
